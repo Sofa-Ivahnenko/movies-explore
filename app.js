fae341fa6042a78efa5987556897a21c351fb722
Sofa Ivahnenko <109969162+Sofa-Ivahnenko@users.noreply.github.com>
2023-06-21 16:04:37 +0300

    fjk

--- app.js
+++ app.js
@@ -31,35 +31,35 @@ const allowedCors = [
   'https://api.diplom.movies.nomoredomains.work',
 ];
 
-// безопасность
-app.use((req, res, next) => {
-  // Сохраняем источник запроса в переменную origin и
-  // проверяем, что источник запроса есть среди разрешённых
-  const { origin } = req.headers;
-  const { method } = req; // Сохраняем тип запроса (HTTP-метод) в соответствующую переменную
-  const requestHeaders = req.headers['access-control-request-headers'];
-  // Значение для заголовка Access-Control-Allow-Methods по умолчанию (разрешены все типы запросов)
-  const DEFAULT_ALLOWED_METHODS = 'GET,HEAD,PUT,PATCH,POST,DELETE';
-  if (allowedCors.includes(origin)) {
-    // устанавливаем заголовок, который разрешает браузеру запросы с этого источника
-    res.header('Access-Control-Allow-Origin', origin);
-    res.header('Access-Control-Allow-Credentials', true);
-  }
-  // Если это предварительный запрос, добавляем нужные заголовки
-  if (method === 'OPTIONS') {
-    // разрешаем кросс-доменные запросы любых типов (по умолчанию)
-    res.header('Access-Control-Allow-Methods', DEFAULT_ALLOWED_METHODS);
-    // завершаем обработку запроса и возвращаем результат клиенту
-    res.header('Access-Control-Allow-Headers', requestHeaders);
-    // завершаем обработку запроса и возвращаем результат клиенту
-    res.end();
-    return;
-  }
-  next();
-});
+// // безопасность
+// app.use((req, res, next) => {
+//   // Сохраняем источник запроса в переменную origin и
+//   // проверяем, что источник запроса есть среди разрешённых
+//   const { origin } = req.headers;
+//   const { method } = req; // Сохраняем тип запроса (HTTP-метод) в соответствующую переменную
+//   const requestHeaders = req.headers['access-control-request-headers'];
+//   // Значение для заголовка Access-Control-Allow-Methods по умолчанию (разрешены все типы запросов)
+//   const DEFAULT_ALLOWED_METHODS = 'GET,HEAD,PUT,PATCH,POST,DELETE';
+//   if (allowedCors.includes(origin)) {
+//     // устанавливаем заголовок, который разрешает браузеру запросы с этого источника
+//     res.header('Access-Control-Allow-Origin', origin);
+//     res.header('Access-Control-Allow-Credentials', true);
+//   }
+//   // Если это предварительный запрос, добавляем нужные заголовки
+//   if (method === 'OPTIONS') {
+//     // разрешаем кросс-доменные запросы любых типов (по умолчанию)
+//     res.header('Access-Control-Allow-Methods', DEFAULT_ALLOWED_METHODS);
+//     // завершаем обработку запроса и возвращаем результат клиенту
+//     res.header('Access-Control-Allow-Headers', requestHeaders);
+//     // завершаем обработку запроса и возвращаем результат клиенту
+//     res.end();
+//     return;
+//   }
+//   next();
+// });
 
-app.use('/', userRoutes); // запускаем импортированные роуты
-app.use('/', movieRoutes); // запускаем импортированные роуты
+// app.use('/', userRoutes); // запускаем импортированные роуты
+// app.use('/', movieRoutes); // запускаем импортированные роуты
 
 mongoose.connect('mongodb://127.0.0.1/bitfilmsdb');
 

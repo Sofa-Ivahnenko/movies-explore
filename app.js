7b6fd7f61c5e92ec1d16c90fb613442f1b3b296c
Sofa Ivahnenko <109969162+Sofa-Ivahnenko@users.noreply.github.com>
2023-06-21 14:08:45 +0300

    level-1

--- app.js
+++ app.js
@@ -61,7 +61,7 @@ app.use((req, res, next) => {
 app.use('/', userRoutes); // запускаем импортированные роуты
 app.use('/', movieRoutes); // запускаем импортированные роуты
 
-mongoose.connect('mongodb://localhost:27017/bitfilmsdb');
+mongoose.connect('mongodb://127.0.0.1/bitfilmsdb');
 
 app.use((req, res, next) => {
   next(new NotFoundError('Страница не найдена'));
